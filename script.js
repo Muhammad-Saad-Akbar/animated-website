bbbf7a9b4eb678031de1ee023d34a18bd0748c85
SaadAkbarGithub <123985914+SaadAkbarGithub@users.noreply.github.com>
2025-10-22 01:31:59 +0500

    improved website responsiveness for all devices

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+
+window.addEventListener("load", () => {
+    const counter = { value: 0 };
+    const percentText = document.querySelector("#percent");
+    const tl = gsap.timeline();
+
+    // Curtain slides up
+    // tl.to(counter, {
+    //     value: 100,
+    //     duration: 2,
+    //     ease: "power4.inOut",
+    //     onUpdate: () => {
+    //         percentText.textContent = Math.floor(counter.value) + "%";
+    //     }
+    // });
+    //
+    // tl.to("#preloader", {
+    //     yPercent: -100,
+    //     delay: 0.5,
+    //     opacity: 0.8,
+    //     duration: 1.5,
+    //     ease: "power4.inOut"
+    // })
+    tl.from('#nav', {
+        y: '-10',
+        delay: -0.8,
+        duration: 1.5,
+        opacity: 0,
+        ease: Expo.easeInOut
+    })
+        .to('.text_animation', {
+            y: '0',
+            duration: 2,
+            delay: -1.1,
+            stagger: 0.2,
+            ease: Expo.easeInOut
+        })
+        .from('#footer', {
+            y: '-10',
+            duration: 1.5,
+            opacity: 0,
+            delay: -1.1,
+            ease: Expo.easeInOut
+        })
+
+    // Allow scrolling again after animation
+    tl.set("body", {
+        overflow: "auto"
+    });
+});
+
+
+
 // Smooth Scrolling
 
 const scrollContainer = document.querySelector("#main");
@@ -23,7 +76,7 @@ ScrollTrigger.scrollerProxy(scrollContainer, {
     pinType: scrollContainer.style.transform ? "transform" : "fixed"
 });
 
-// 🔁 Ensure ScrollTrigger updates on Locomotive scroll
+//  Ensure ScrollTrigger updates on Locomotive scroll
 scroll.on("scroll", ScrollTrigger.update);
 
 // Refresh ScrollTrigger after Locomotive has set everything up
@@ -58,34 +111,9 @@ function customCursorAnimation() {
 
         customCursor(xScale, yScale);
 
-         timeout = setTimeout(function () {
+        timeout = setTimeout(function () {
             document.querySelector('#mini_circle').style.transform = `translate(${details.clientX}px, ${details.clientY}px) scale(1, 1)`
         }, 100)
-    })
-}
-
-// home section animation
-function firstSectionAnimation() {
-    let t1 = gsap.timeline();
-    t1.from('#nav', {
-        y: '-10',
-        duration: 1.5,
-        opacity: 0,
-        ease: Expo.easeInOut
-    })
-    t1.to('.text_animation', {
-        y: '0',
-        duration: 2,
-        delay: -1,
-        stagger: 0.2,
-        ease: Expo.easeInOut
-    })
-    t1.from('#footer', {
-        y: '-10',
-        duration: 1.5,
-        opacity: 0,
-        delay: -1,
-        ease: Expo.easeInOut
     })
 }
 
@@ -116,14 +144,40 @@ document.querySelectorAll('.elem').forEach(function (elem) {
             y: diffTop - imgHeight / 2,
             ease: Power3
         })
-       secondTimeOut =  setTimeout(function () {
-           gsap.to(elem.querySelector('.img'), {
-               rotate: 0,
-               ease: Power1
-           })
+        secondTimeOut =  setTimeout(function () {
+            gsap.to(elem.querySelector('.img'), {
+                rotate: 0,
+                ease: Power1
+            })
         }, 100)
     })
 })
+// ScrollTrigger.matchMedia({
+//     "(max-width: 767px)": function () {
+//         document.querySelectorAll(".waveCard").forEach(function (card, index) {
+//             // Create the tween and store it in a variable
+//             const floatTween = gsap.to(card, {
+//                 y: -20,
+//                 duration: 1.5,
+//                 ease: "power1.inOut",
+//                 repeat: -1,
+//                 yoyo: true,
+//                 paused: true,
+//                 delay: index * 0.3
+//             });
+//
+//             // Create a ScrollTrigger that controls it
+//             ScrollTrigger.create({
+//                 trigger: card,
+//                 start: "top 30%",
+//                 markers: true,
+//                 scroller: '#main',
+//                 onEnter: () => floatTween.play(),
+//                 onLeaveBack: () => floatTween.pause()
+//             });
+//         })
+//     }
+// });
 
 // Third section animation
 function animationOnThirdSection() {
@@ -146,6 +200,7 @@ function animationOnAboutSection() {
         scrollTrigger: {
             trigger: '#content',
             start: '90% top',
+            markers: true,
             scroller: '#main'
         }
     })
@@ -158,6 +213,7 @@ function animationOnSubscribeSection() {
         scrollTrigger: {
             trigger: '#companies_logo',
             start: 'center top',
+            markers: true,
             scroller: '#main'
         }
     })
@@ -166,7 +222,6 @@ function animationOnSubscribeSection() {
 
 customCursor();
 customCursorAnimation();
-firstSectionAnimation();
 animationOnThirdSection();
 animationOnAboutSection();
 animationOnSubscribeSection();
